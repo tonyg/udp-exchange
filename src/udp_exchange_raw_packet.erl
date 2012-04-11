1003f4c1c10e23164ba52954513491c6123af625
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2012-04-11 08:12:34 -0400

    Remove ad-hoc, redundant source_ip and source_port headers.

--- src/udp_exchange_raw_packet.erl
+++ src/udp_exchange_raw_packet.erl
@@ -6,12 +6,10 @@
 configure(#exchange{}) ->
     no_config.
 
-parse({A, B, C, D}, Port, Packet, _Config) ->
-    IpStr = list_to_binary(io_lib:format("~p.~p.~p.~p", [A, B, C, D])),
+parse(_IpAddr, _Port, Packet, _Config) ->
     %% FIXME: use more clever end-of-routing key detection
     {ok, {udp_exchange:truncate_bin(255, Packet),
-          [{headers, [{<<"source_ip">>, longstr, IpStr},
-                      {<<"source_port">>, signedint, Port}]}],
+          [],
           Packet}}.
 
 format(IpAddr, Port, _RoutingKeySuffixes, Body, #delivery{}, _Config) ->
