2e19996f7039c24dac6828c1e7949c147b3d6e02
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2013-07-06 13:56:56 -0400

    Latest RabbitMQ plugin API changes.

--- src/udp_exchange.erl
+++ src/udp_exchange.erl
@@ -18,8 +18,9 @@
                     {enables,     kernel_ready}]}).
 
 -export([description/0, serialise_events/0, route/2]).
--export([validate/1, create/2, delete/3, policy_changed/3, add_binding/3,
-	 remove_bindings/3, assert_args_equivalence/2]).
+-export([validate/1, validate_binding/2, create/2, delete/3,
+         policy_changed/2,
+         add_binding/3, remove_bindings/3, assert_args_equivalence/2]).
 
 -export([truncate_bin/2]). %% utility
 
@@ -39,6 +40,9 @@ validate(X) ->
     _ = endpoint_params(X),
     ok.
 
+%% Called BEFORE declaration, to check args etc
+validate_binding(_X, _B) -> ok.
+
 %% Called just the first time the exchange is declared.
 create(transaction, X) ->
     ensure_relay_exists(X),
@@ -53,7 +57,7 @@ delete(transaction, X, _Bs) ->
 delete(none, _X, _Bs) ->
     ok.
 
-policy_changed(_Tx, _X1, _X2) -> ok.
+policy_changed(_X1, _X2) -> ok.
 
 add_binding(Tx, X, B) -> rabbit_exchange_type_topic:add_binding(Tx, X, B).
 remove_bindings(Tx, X, Bs) -> rabbit_exchange_type_topic:remove_bindings(Tx, X, Bs).
